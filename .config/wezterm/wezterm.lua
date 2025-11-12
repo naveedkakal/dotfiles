4031b2d68f7c8713c5ad8939cbecc0584a4c5c3b
Naveed Kakal <naveed.kakal@gmail.com>
2025-11-12 00:46:37 -0600

    Refactor wezterm config to use consistent style
    Updated wezterm.lua to use consistent double-quoted strings and table formatting throughout. Function calls now use parentheses for arguments, and keybinding actions are updated for clarity and uniformity. Added a new keybinding for Shift+Enter to send an escape sequence.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,12 +3,12 @@
 -- https://wezfurlong.org/wezterm/
 -- ============================================================================
 
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = {}
 
 -- Use config builder for better error reporting
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- ============================================================================
@@ -16,31 +16,31 @@ end
 -- ============================================================================
 
 -- Color scheme
-config.color_scheme = 'Tokyo Night'
+config.color_scheme = "Tokyo Night"
 
 -- Font configuration
-config.font = wezterm.font('Hack Nerd Font Mono', { weight = 'Regular' })
+config.font = wezterm.font("Hack Nerd Font Mono", { weight = "Regular" })
 config.font_size = 15.0
 config.line_height = 1.2
-config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
 -- Window appearance
-config.window_decorations = 'RESIZE'
+config.window_decorations = "RESIZE"
 config.window_frame = {
-  border_left_width = '0.25cell',
-  border_right_width = '0.25cell',
-  border_bottom_height = '0.15cell',
-  border_top_height = '0.15cell',
-  border_left_color = '#444444',
-  border_right_color = '#444444',
-  border_bottom_color = '#444444',
-  border_top_color = '#444444',
+	border_left_width = "0.25cell",
+	border_right_width = "0.25cell",
+	border_bottom_height = "0.15cell",
+	border_top_height = "0.15cell",
+	border_left_color = "#444444",
+	border_right_color = "#444444",
+	border_bottom_color = "#444444",
+	border_top_color = "#444444",
 }
 config.window_padding = {
-  left = 10,
-  right = 10,
-  top = 10,
-  bottom = 5,
+	left = 10,
+	right = 10,
+	top = 10,
+	bottom = 5,
 }
 
 -- Tab bar
@@ -70,12 +70,12 @@ config.scrollback_lines = 10000
 
 -- Mouse
 config.mouse_bindings = {
-  -- Open URLs with Cmd+Click
-  {
-    event = { Up = { streak = 1, button = 'Left' } },
-    mods = 'CMD',
-    action = wezterm.action.OpenLinkAtMouseCursor,
-  },
+	-- Open URLs with Cmd+Click
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "CMD",
+		action = wezterm.action.OpenLinkAtMouseCursor,
+	},
 }
 
 -- ============================================================================
@@ -83,97 +83,104 @@ config.mouse_bindings = {
 -- ============================================================================
 
 config.keys = {
-  -- Word and line navigation (macOS style)
-  {
-    key = 'LeftArrow',
-    mods = 'OPT',
-    action = wezterm.action.SendKey { key = 'b', mods = 'ALT' },
-  },
-  {
-    key = 'RightArrow',
-    mods = 'OPT',
-    action = wezterm.action.SendKey { key = 'f', mods = 'ALT' },
-  },
-  {
-    key = 'LeftArrow',
-    mods = 'CMD',
-    action = wezterm.action.SendKey { key = 'a', mods = 'CTRL' },
-  },
-  {
-    key = 'RightArrow',
-    mods = 'CMD',
-    action = wezterm.action.SendKey { key = 'e', mods = 'CTRL' },
-  },
-  {
-    key = 'Backspace',
-    mods = 'CMD',
-    action = wezterm.action.SendKey { key = 'u', mods = 'CTRL' },
-  },
+	-- Word and line navigation (macOS style)
+	{
+		key = "LeftArrow",
+		mods = "OPT",
+		action = wezterm.action.SendKey({ key = "b", mods = "ALT" }),
+	},
+	{
+		key = "RightArrow",
+		mods = "OPT",
+		action = wezterm.action.SendKey({ key = "f", mods = "ALT" }),
+	},
+	{
+		key = "LeftArrow",
+		mods = "CMD",
+		action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
+	},
+	{
+		key = "RightArrow",
+		mods = "CMD",
+		action = wezterm.action.SendKey({ key = "e", mods = "CTRL" }),
+	},
+	{
+		key = "Backspace",
+		mods = "CMD",
+		action = wezterm.action.SendKey({ key = "u", mods = "CTRL" }),
+	},
 
-  -- Splits
-  {
-    key = 'd',
-    mods = 'CMD',
-    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-  },
-  {
-    key = 'd',
-    mods = 'CMD|SHIFT',
-    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-  },
+	-- Splits
+	{
+		key = "d",
+		mods = "CMD",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		key = "d",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
 
-  -- Navigate splits
-  {
-    key = 'h',
-    mods = 'CMD|SHIFT',
-    action = wezterm.action.ActivatePaneDirection 'Left',
-  },
-  {
-    key = 'l',
-    mods = 'CMD|SHIFT',
-    action = wezterm.action.ActivatePaneDirection 'Right',
-  },
-  {
-    key = 'k',
-    mods = 'CMD|SHIFT',
-    action = wezterm.action.ActivatePaneDirection 'Up',
-  },
-  {
-    key = 'j',
-    mods = 'CMD|SHIFT',
-    action = wezterm.action.ActivatePaneDirection 'Down',
-  },
+	-- Navigate splits
+	{
+		key = "h",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		key = "l",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		key = "k",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		key = "j",
+		mods = "CMD|SHIFT",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
 
-  -- Close pane
-  {
-    key = 'w',
-    mods = 'CMD',
-    action = wezterm.action.CloseCurrentPane { confirm = true },
-  },
+	-- Close pane
+	{
+		key = "w",
+		mods = "CMD",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
 
-  -- Clear scrollback
-  {
-    key = 'k',
-    mods = 'CMD',
-    action = wezterm.action.ClearScrollback 'ScrollbackAndViewport',
-  },
+	-- Clear scrollback
+	{
+		key = "k",
+		mods = "CMD",
+		action = wezterm.action.ClearScrollback("ScrollbackAndViewport"),
+	},
 
-  -- Font size
-  {
-    key = '+',
-    mods = 'CMD',
-    action = wezterm.action.IncreaseFontSize,
-  },
-  {
-    key = '-',
-    mods = 'CMD',
-    action = wezterm.action.DecreaseFontSize,
-  },
-  {
-    key = '0',
-    mods = 'CMD',
-    action = wezterm.action.ResetFontSize,
-  },
+	-- Font size
+	{
+		key = "+",
+		mods = "CMD",
+		action = wezterm.action.IncreaseFontSize,
+	},
+	{
+		key = "-",
+		mods = "CMD",
+		action = wezterm.action.DecreaseFontSize,
+	},
+	{
+		key = "0",
+		mods = "CMD",
+		action = wezterm.action.ResetFontSize,
+	},
+
+	-- Claude Shift Enter
+	{
+		key = "Enter",
+		mods = "SHIFT",
+		action = wezterm.action({ SendString = "\x1b\r" }),
+	},
 }
 
 -- ============================================================================
@@ -181,7 +188,7 @@ config.keys = {
 -- ============================================================================
 
 -- Default shell
-config.default_prog = { '/bin/zsh', '-l' }
+config.default_prog = { "/bin/zsh", "-l" }
 
 -- ============================================================================
 -- Return Configuration
