987aaa3a4e0ce32bb3358268303d0e67ace1e3ff
Naveed Kakal <naveed.kakal@gmail.com>
2026-06-06 13:48:29 -0500

    fixing nvim linux

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -936,6 +936,7 @@ require('lazy').setup({
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    branch = 'master', -- Pin to the legacy API branch; the new 'main' branch drops nvim-treesitter.configs
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
